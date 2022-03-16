5f2c6ced0c5ac0486c5c8ad7cd24b4cc75b4f321
bokhtiar <bokhtiartoshar1@gmail.com>
2022-03-16 13:56:33 +0600

    recommencted product

--- src/Components/Modules/Menu/SingleProduct.jsx
+++ src/Components/Modules/Menu/SingleProduct.jsx
@@ -23,7 +23,7 @@ export default function SingleProduct() {
       <section className="breadcrumbs">
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center">
-                <h7>ABOUT-RESTAURANTLY</h7>
+                <h5>ABOUT-RESTAURANTLY</h5>
                 <ol>
                     <li><Link to={'/'} >Home</Link></li>
                     <li>About</li>
@@ -51,7 +51,68 @@ export default function SingleProduct() {
                         </div>
                     </div>
                 </div>
-                
+
+                <section id="why-us" className="why-us">
+            <div className="container" data-aos="fade-up">
+
+                <div className="section-title">
+                <h2>Recommented Product</h2>
+                <p>Category Releted Product</p>
+                </div>
+
+                <div className="row">
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                       <div className="card">
+                       <img class="card-img-top" src="https://picsum.photos/100/100" alt="Card image cap"/>
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-dark">Card title</h5>
+                            <a href="#" class="btn btn-primary">Detail</a> &nbsp;
+                            <a href="#" class="btn btn-primary">buy</a> &nbsp;
+                            <a href="#" class="btn btn-primary">Wishlist</a>
+                        </div>
+                       </div>
+                    </div>
+
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                       <div className="card">
+                       <img class="card-img-top" src="https://picsum.photos/100/100" alt="Card image cap"/>
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-dark">Card title</h5>
+                            <a href="#" class="btn btn-primary">Detail</a> &nbsp;
+                            <a href="#" class="btn btn-primary">buy</a> &nbsp;
+                            <a href="#" class="btn btn-primary">Wishlist</a>
+                        </div>
+                       </div>
+                    </div>
+                    
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                       <div className="card">
+                       <img class="card-img-top" src="https://picsum.photos/100/100" alt="Card image cap"/>
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-dark">Card title</h5>
+                            <a href="#" class="btn btn-primary">Detail</a> &nbsp;
+                            <a href="#" class="btn btn-primary">buy</a> &nbsp;
+                            <a href="#" class="btn btn-primary">Wishlist</a>
+                        </div>
+                       </div>
+                    </div>
+
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                       <div className="card">
+                       <img class="card-img-top" src="https://picsum.photos/100/100" alt="Card image cap"/>
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-dark">Card title</h5>
+                            <a href="#" class="btn btn-primary">Detail</a> &nbsp;
+                            <a href="#" class="btn btn-primary">buy</a> &nbsp;
+                            <a href="#" class="btn btn-primary">Wishlist</a>
+                        </div>
+                       </div>
+                    </div>
+
+                </div>
+
+            </div>
+            </section>
             </div>
         </section>
     </div>
