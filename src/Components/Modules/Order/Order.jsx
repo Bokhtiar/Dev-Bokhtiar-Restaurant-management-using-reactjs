3dff96e4b96ae3059bc556e94bc913bbcfb2f14f
bokhtiar <bokhtiartoshar1@gmail.com>
2022-03-29 14:52:31 +0600

    order page design done

--- src/Components/Modules/Order/Order.jsx
+++ src/Components/Modules/Order/Order.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import CartList from '../Cart/CartList'
 
 export default function Order() {
+	let name = localStorage.getItem('user_id')
+	let email = localStorage.getItem('email')
   return (
     <div>
-      <section id="cart_items my-5">
+		<br />
+      <section id="cart_items my-5 mt-3">
 		<div class="container my-5">
 			<div class="register-req">
 				<p>Please use Register And Checkout to easily get access to your order history, or use Checkout as Guest</p>
@@ -16,74 +19,39 @@ export default function Order() {
 						<div class="shopper-info">
 							<p>Shopper Information</p>
 							<form>
-								<input className='form-control my-2' type="text" placeholder="Display Name"/>
-								<input className='form-control my-2' type="text" placeholder="User Name"/>
-								<input className='form-control my-2' type="password" placeholder="Password"/>
-								<input className='form-control my-2' type="password" placeholder="Confirm password"/>
+								<input className='form-control my-2' value={name} type="text" placeholder="Display Name"/>
+								<input className='form-control my-2' value={email} type="text" placeholder="User Name"/>
+								<input className='form-control my-2' type="number" value={'0198329232'} placeholder="phone"/>
 							</form>
-							<a class="btn btn-primary" href="">Get Quotes</a>
-							<a class="btn btn-primary" href="">Continue</a>
 						</div>
 					</div>
 					<div class="col-sm-5 clearfix">
 						<div class="bill-to">
-							<p>Bill To</p>
+							<p>Reciver To</p>
 							<div class="form-one">
 								<form>
-									<input className='form-control my-2' type="text" placeholder="Company Name"/>
-									<input className='form-control my-2' type="text" placeholder="Email*"/>
-									<input className='form-control my-2' type="text" placeholder="Title"/>
 									<input className='form-control my-2' type="text" placeholder="First Name *"/>
 									<input className='form-control my-2' type="text" placeholder="Middle Name"/>
 									<input className='form-control my-2' type="text" placeholder="Last Name *"/>
+									<input className='form-control my-2' type="text" placeholder="Email*"/>
+									<input className='form-control my-2' type="number" placeholder="phone"/>
 									<input className='form-control my-2' type="text" placeholder="Address 1 *"/>
 									<input className='form-control my-2' type="text" placeholder="Address 2"/>
 								</form>
 							</div>
-							<div class="form-two">
-								<form>
-									<input className='form-control my-2' type="text" placeholder="Zip / Postal Code *"/>
-									<select className='form-control my-2'>
-										<option>-- Country --</option>
-										<option>United States</option>
-										<option>Bangladesh</option>
-										<option>UK</option>
-										<option>India</option>
-										<option>Pakistan</option>
-										<option>Ucrane</option>
-										<option>Canada</option>
-										<option>Dubai</option>
-									</select>
-									<select className='form-control my-2'>
-										<option>-- State / Province / Region --</option>
-										<option>United States</option>
-										<option>Bangladesh</option>
-										<option>UK</option>
-										<option>India</option>
-										<option>Pakistan</option>
-										<option>Ucrane</option>
-										<option>Canada</option>
-										<option>Dubai</option>
-									</select>
-									<input className='form-control my-2' type="password" placeholder="Confirm password"/>
-									<input className='form-control my-2' type="text" placeholder="Phone *"/>
-									<input className='form-control my-2' type="text" placeholder="Mobile Phone"/>
-									<input className='form-control my-2' type="text" placeholder="Fax"/>
-								</form>
-							</div>
+						
 						</div>
 					</div>
 					<div class="col-sm-4">
 						<div class="order-message">
 							<p>Shipping Order</p>
-							<textarea className='form-control my-2' name="message"  placeholder="Notes about your order, Special Notes for Delivery" rows="16"></textarea>
-							<label><input type="checkbox"/> Shipping to bill address</label>
+							<textarea className='form-control my-2' name="message"  placeholder="Notes about your order, Special Notes for Delivery" rows="12"></textarea>
 						</div>	
 					</div>					
 				</div>
 			</div>
 			<div class="review-payment">
-				<h2>Review & Payment</h2>
+				<h2> Payment</h2>
 			</div>
 
 			
